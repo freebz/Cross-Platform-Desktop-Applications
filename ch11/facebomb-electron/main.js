db4be8cee3730c79b059c68cfaed1f50fb25a375
freebz <freebz.dev@gmail.com>
2021-08-12 09:42:06 +0900

    Chapter 11 웹캠 활용하기

---
+++ ch11/facebomb-electron/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    useContentSize: true,
+    width: 800,
+    height: 600,
+    resizable: false,
+    fullscreen: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    }    
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
+});
