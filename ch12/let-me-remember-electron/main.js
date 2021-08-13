e6523438b2f640f100dfa6210aa10b20bf2ae30a
freebz <freebz.dev@gmail.com>
2021-08-13 09:46:20 +0900

    Chapter 12 애플리케이션의 데이터 저장하기

---
+++ ch12/let-me-remember-electron/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 480,
+    height: 320,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    }    
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
+});
