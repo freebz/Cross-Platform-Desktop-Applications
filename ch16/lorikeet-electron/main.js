adf9e84f88b4147572e3b00729202aeb14bcaefe
freebz <freebz.dev@gmail.com>
2021-08-19 10:38:47 +0900

    Chapter 16 테스트

---
+++ ch16/lorikeet-electron/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+      webPreferences: {
+	nodeIntegration: true,
+	contextIsolation: false,
+      }
+    });
+  mainWindow.loadURL(`file://${app.getAppPath()}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
+});
